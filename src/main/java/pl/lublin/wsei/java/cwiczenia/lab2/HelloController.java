9f7cf9827df8544050607b28eb9b81fdc06a9fe3
Mariusz Dłubak <maniek.14.12.2@gmail.com>
2022-03-28 20:40:42 +0200

    Dodanie pola tekstowego do GUI oraz wyświetlania w nim licznika kliknięć

--- src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
+++ src/main/java/pl/lublin/wsei/java/cwiczenia/lab2/HelloController.java
@@ -2,13 +2,15 @@ package pl.lublin.wsei.java.cwiczenia.lab2;
 
 import javafx.event.ActionEvent;
 import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
 
 public class HelloController {
     public Button btnMain;
+    public TextField txtLicznik;
     int clickCounter = 0;
 
     public void onBtnAction(ActionEvent actionEvent) {
-        System.out.println("Kliknięcie nr " + ++clickCounter);
+        txtLicznik.setText("Kliknięcie nr " + ++clickCounter);
         if (clickCounter == 1)
             btnMain.setText("I jeszcze raz");
     }
